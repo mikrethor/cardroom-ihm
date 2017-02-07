41804899851611a27b60c18730cad7a1b7a6a4e1
Xavier BOUCLET <mikrethor@gmail.com>
2017-02-06 21:00:59 -0500

    Test to get a list

--- cardroom-app/src/main/java/fr/mikrethor/cardroom/app/handlers/TestWsHandler.java
+++ cardroom-app/src/main/java/fr/mikrethor/cardroom/app/handlers/TestWsHandler.java
@@ -1,5 +1,7 @@
 package fr.mikrethor.cardroom.app.handlers;
 
+import java.util.List;
+
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.workbench.IWorkbench;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -7,12 +9,13 @@ import org.eclipse.swt.widgets.Shell;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.api.json.JSONConfiguration;
 
-import fr.mikrethor.cardroom.app.objects.Hand;
+import fr.mikrethor.cardroom.pojo.Account;
 
 public class TestWsHandler {
 	@Execute
@@ -23,19 +26,30 @@ public class TestWsHandler {
 			ClientConfig clientConfig = new DefaultClientConfig();
 			clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
 			Client client = Client.create(clientConfig);
-			// Client client = Client.create();
 
-			WebResource webResource = client.resource("http://localhost:8080/hand");
+			WebResource webResource = client.resource("http://localhost:8080/accounts");
 			ClientResponse response = webResource.get(ClientResponse.class);
-			// ClientResponse response = webResource.accept("application/json")
-			// .get(ClientResponse.class);
+
+			// WebResource webResource =
+			// client.resource("http://localhost:8080/hand");
+			// ClientResponse response = webResource.get(ClientResponse.class);
+			// if (response.getStatus() != 200) {
+			// throw new RuntimeException("Failed : HTTP error code : " +
+			// response.getStatus());
+			// }
+			//
+			// Hand output = response.getEntity(Hand.class);
 
 			if (response.getStatus() != 200) {
 				throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
 			}
-
-			Hand output = response.getEntity(Hand.class);
-			result = output.toString();
+			List<Account> output = response.getEntity(new GenericType<List<Account>>() {
+			});
+			result = "";
+			for (Account a : output) {
+				result = result + a;
+				System.out.println(a.getPlayer().getName());
+			}
 			System.out.println("Output from Server .... \n");
 			System.out.println(output);
 
